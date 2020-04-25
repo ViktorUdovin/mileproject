c9799654311fb9468ad4f41152c74dc814e1cb22
ViktorUdovin <viktorudovinwork@gmail.com>
2020-04-25 22:30:49 +0400

    added all flies

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 2777;
+        int bonus = 20;
+        int mile = ticket / bonus;
+        System.out.println(mile);
+    }
+}
