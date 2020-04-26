d23c4b73a9012fc939ba228b710657c510309395
ViktorUdovin <viktorudovinwork@gmail.com>
2020-04-26 15:53:34 +0400

    Code edit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int ticket = 2777;
         int bonus = 20;
-        int mile = ticket / bonus;
+        int miles = ticket / bonus;
         System.out.println(mile);
     }
 }
